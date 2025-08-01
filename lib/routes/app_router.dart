c3f9fc7d55c6f9b85ec01dbc5c7649135ac641c1
hamdi-ab <hamdiabdulfetah22@gmail.com>
2025-08-01 10:34:51 +0300

    fixed the route

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,12 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive/hive.dart';
 
 import '../models/coin_adapter.dart';
-import '../ui/screens/details_page.dart';
-import '../ui/screens/home_page.dart';
-import '../ui/screens/wishlist_page.dart';
+import '../features/details/details_page.dart';
+import '../features/home/home_page.dart';
+import '../features/wishlist/wishlist_page.dart';
 
 class AppRouter {
   final GoRouter router = GoRouter(
